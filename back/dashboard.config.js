718d5ae97c46bed551b327283df68e7b17df47b3
Gireg <gireg.dekerdanet@hotmail.fr>
2021-10-19 15:36:58 +0200

    fix pm2 config file

--- back/dashboard.config.js
+++ back/dashboard.config.js
@@ -1,16 +1,16 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '.env') });
+require('dotenv').config({ path: path.join(__dirname, '../.env') });
 
 module.exports = {
   apps: [
     {
       name: 'upsignon-pro-dashboard',
-      script: './compiledServer/server.js',
+      script: path.join(__dirname, './compiledServer/server.js'),
       instances: 1,
       exec_model: 'fork',
-      error_file: '../logs/server-error.log',
-      out_file: '../logs/server-output.log',
+      error_file: path.join(__dirname, '../logs/server-error.log'),
+      out_file: path.join(__dirname, '../logs/server-output.log'),
       combine_logs: true,
       kill_timeout: 3000,
       source_map_support: true,
