562c46c67162817324ea462b1611fbef7d37534c
Gireg <gireg.dekerdanet@hotmail.fr>
2021-10-19 16:50:23 +0200

    improve disconnect mechanism

--- back/server/api/disconnect.ts
+++ back/server/api/disconnect.ts
@@ -1,11 +1,6 @@
-import { db } from '../helpers/connection';
-
 export const disconnect = async (req: any, res: any): Promise<void> => {
   try {
-    const adminEmail = req.session?.adminEmail;
-    await db.query(`DELETE FROM admin_sessions WHERE session_data ->> 'adminEmail' = $1`, [
-      adminEmail,
-    ]);
+    req.session.destroy();
     res.status(200).end();
   } catch (e) {
     console.error(e);
