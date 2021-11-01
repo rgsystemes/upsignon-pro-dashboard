2c873bdc170b0b254d41a84507c0b7a4a046bda6
Gireg <gireg.dekerdanet@hotmail.fr>
2021-11-01 17:44:27 +0100

    add comments in dot-env-example

--- update.sh
+++ update.sh
@@ -1,5 +1,4 @@
 git pull origin production --ff-only
-mkdir logs
 cd front
 yarn install
 yarn build-front
@@ -8,3 +7,4 @@ yarn install
 yarn build-server
 cd ..
 pm2 startOrReload ./back/dashboard.config.js
+pm2 save
