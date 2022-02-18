fbfb9473c33268ddd640925814a13a7df4442aac
Gireg <gireg.dekerdanet@hotmail.fr>
2022-02-18 12:14:09 +0100

    allow connection with db id as login

--- back/server/login/manualConnect.ts
+++ back/server/login/manualConnect.ts
@@ -12,7 +12,14 @@ export const manualConnect = async (req: any, res: any): Promise<void> => {
     try {
       dbRes = await db.query('SELECT password_hash FROM admins WHERE email=$1', [email]);
     } catch {}
-    if (!dbRes || dbRes.rowCount === 0) return res.status(401).end();
+    if (!dbRes || dbRes.rowCount === 0) {
+      try {
+        dbRes = await db.query('SELECT password_hash FROM admins WHERE id=$1', [email]);
+      } catch {}
+      if (!dbRes || dbRes.rowCount === 0) {
+        return res.status(401).end();
+      }
+    }
     const isOk: boolean = await bcrypt.compare(pwd, dbRes.rows[0].password_hash);
     console.log('checking password', isOk);
     if (!isOk) return res.status(401).end();
