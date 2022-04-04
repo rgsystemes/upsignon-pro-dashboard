f8be325785378bbf9137de8d370d6499f5ad1999
Gireg <gireg.dekerdanet@hotmail.fr>
2022-04-04 17:09:44 +0200

    fix

--- back/server/helpers/redirectToDefaultPath.ts
+++ back/server/helpers/redirectToDefaultPath.ts
@@ -3,11 +3,11 @@ import env from './env';
 export const redirectToDefaultPath = (req: any, res: any): void => {
   let defaultPath = '';
   if (req.session.isSuperadmin) {
-    defaultPath = 'superadmin';
+    defaultPath = 'superadmin/'; // Keep trailing '/' for cases where dashboard url has a path !
   } else if (!req.session.groups || req.session.groups.length === 0) {
     defaultPath = 'no-admin-bank.html';
   } else {
-    defaultPath = req.session.groups[0];
+    defaultPath = req.session.groups[0] + '/'; // Keep trailing '/' for cases where dashboard url has a path !
   }
 
   if (env.IS_PRODUCTION) {
