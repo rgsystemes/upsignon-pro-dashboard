e7516f48893b041e712ea89d50c5f152a0949254
Gireg <gireg.dekerdanet@hotmail.fr>
2023-03-10 12:05:35 +0100

    fix update script

--- post-update.sh
+++ post-update.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd front
-/home/upsignonpro/.npm-global/bin/yarn install
+~/.npm-global/bin/yarn install
 ./buildFront.sh
 
 
 cd ../back
-/home/upsignonpro/.npm-global/bin/yarn install
-/home/upsignonpro/.npm-global/bin/yarn build-server
+~/.npm-global/bin/yarn install
+~/.npm-global/bin/yarn build-server
 cd ..
 
-/home/upsignonpro/.npm-global/bin/pm2 stop upsignon-pro-dashboard
-/home/upsignonpro/.npm-global/bin/pm2 start ./back/dashboard.config.js --update-env
+~/.npm-global/bin/pm2 stop upsignon-pro-dashboard
+~/.npm-global/bin/pm2 start ./back/dashboard.config.js --update-env
