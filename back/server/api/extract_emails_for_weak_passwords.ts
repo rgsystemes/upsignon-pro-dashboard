d1c4673516fd8dd2cf814627bf45bbe7ae1fe769
Gireg <giregk@upsignon.eu>
2023-07-10 18:18:13 +0200

    improve extract weak request

--- back/server/api/extract_emails_for_weak_passwords.ts
+++ back/server/api/extract_emails_for_weak_passwords.ts
@@ -5,10 +5,7 @@ export const extract_emails_for_weak_passwords = async (req: any, res: any): Pro
   try {
     const nb = parseInt(req.query.minWeak, 10) || 1;
     const dbRes = await db.query(
-      `SELECT email FROM users AS u
-    WHERE (SELECT nb_accounts_weak FROM data_stats AS ds WHERE ds.user_id=u.id ORDER BY date DESC LIMIT 1) >= $1
-    AND u.group_id=$2
-  `,
+      `SELECT email FROM users AS u WHERE nb_accounts_weak >= $1 AND u.group_id=$2`,
       [nb, req.proxyParamsGroupId],
     );
     res.status(200).send(dbRes.rows.map((u) => u.email));
