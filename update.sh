9e8c256a9fd39851bd2bf71e4ac88fe39772ee2f
Gireg <giregk@upsignon.eu>
2023-09-27 00:06:51 +0200

    clean git repo of remote branches in update script

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 git fetch origin
 git reset --hard origin/production
 git clean -df
+git remote prune origin
 
 # Use a separate script so the update immediatly benefits from the new update script
 if [ $? -eq 0 ];
