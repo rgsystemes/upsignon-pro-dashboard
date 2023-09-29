29b44d150283e127d5caa39e3ef45364d71eb5a8
Gireg <giregk@upsignon.eu>
2023-09-29 10:35:30 +0200

    fix group ordering in dashboard menu

--- back/server/helpers/get_available_groups.ts
+++ back/server/helpers/get_available_groups.ts
@@ -3,7 +3,7 @@ import { logError } from './logger';
 
 export const get_available_groups = async (req: any, res: any): Promise<void> => {
   try {
-    const allGroups = await db.query('SELECT id, name FROM groups');
+    const allGroups = await db.query('SELECT id, name FROM groups ORDER BY NAME ASC');
     if (!req.session.isSuperadmin) {
       const filteredGroups = allGroups.rows.filter((g) => req.session.groups.includes(g.id));
       if (filteredGroups.length === 0) {
@@ -16,7 +16,7 @@ export const get_available_groups = async (req: any, res: any): Promise<void> =>
     // superadmin case
     res.status(200).json({ groups: allGroups.rows, isSuperadmin: true });
   } catch (e) {
-    logError("get_available_groups", e);
+    logError('get_available_groups', e);
     res.status(400).end();
   }
 };
