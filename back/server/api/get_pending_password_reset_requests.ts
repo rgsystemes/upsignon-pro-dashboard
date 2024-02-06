29c4b0b8c2c652bb168892745ca2db26b546f3d2
Gireg <giregk@upsignon.eu>
2024-02-06 09:23:34 +0100

    fix password reset request order

--- back/server/api/get_pending_password_reset_requests.ts
+++ back/server/api/get_pending_password_reset_requests.ts
@@ -13,17 +13,17 @@ export const get_pending_password_reset_requests = async (
       u.email,
       ud.device_name,
       ud.device_type AS device_type,
-      reset.status AS status,
-      reset.id AS pwd_reset_id,
-      reset.reset_token AS pwd_reset_token,
-      reset.created_at AS pwd_reset_created_at,
+      prr.status AS status,
+      prr.id AS pwd_reset_id,
+      prr.reset_token AS pwd_reset_token,
+      prr.created_at AS pwd_reset_created_at,
       (SELECT STRING_AGG(users.email,';') FROM user_devices AS udbis INNER JOIN users ON udbis.user_id=users.id WHERE udbis.device_unique_id=ud.device_unique_id AND udbis.id!=ud.id) AS shared_with
-    FROM password_reset_request AS reset
-    INNER JOIN user_devices AS ud ON reset.device_id=ud.id
+    FROM password_reset_request AS prr
+    INNER JOIN user_devices AS ud ON prr.device_id=ud.id
     INNER JOIN users AS u ON ud.user_id=u.id
     INNER JOIN groups ON u.group_id=groups.id
-    ${asSuperadmin ? '' : 'WHERE reset.group_id=$1'}
-    ORDER BY ud.created_at DESC
+    ${asSuperadmin ? '' : 'WHERE prr.group_id=$1'}
+    ORDER BY prr.created_at DESC
     `,
       asSuperadmin ? [] : [req.proxyParamsGroupId],
     );
