500f95e8ed8aa23612e4b10f7f99d4023ee730d3
Gireg <giregk@upsignon.eu>
2024-10-25 15:41:03 +0200

    add logs on get_licences route

--- back/server/api/get_licences.ts
+++ back/server/api/get_licences.ts
@@ -1,5 +1,5 @@
 import { db } from '../helpers/db';
-import { logError } from '../helpers/logger';
+import { logError, logInfo } from '../helpers/logger';
 
 export const get_licences = async (
   req: any,
@@ -7,18 +7,23 @@ export const get_licences = async (
   isSuperadminPage: boolean,
 ): Promise<void> => {
   try {
+    logInfo('get_licences');
     const dbRes = await db.query("SELECT value FROM settings WHERE key='LICENCES'", []);
     const groupsRes = await db.query('SELECT id, name FROM groups');
+    logInfo('get_licences groupsRes', groupsRes);
     const licencesRes: any[] = dbRes.rows[0]?.value.map((l: any) => {
       return {
         ...l,
         bankName: groupsRes.rows.find((g) => g.id === l.masterBank)?.name,
       };
     });
+    logInfo('get_licences licencesRes', licencesRes);
     if (isSuperadminPage) {
+      logInfo('get_licences ok superadmin');
       res.status(200).send(licencesRes);
     } else {
       const groupLicences = licencesRes?.filter((r) => r.masterBank == req.proxyParamsGroupId);
+      logInfo('get_licences ok', req.proxyParamsGroupId, groupLicences);
       res.status(200).send(groupLicences);
     }
   } catch (e) {
