3c97fc9d056c2a3c146a27e9cb3c16f9b9adb277
Gireg <giregk@upsignon.eu>
2024-12-13 23:19:32 +0100

    fix insert group admin

--- back/server/api/insert_group_admin.ts
+++ back/server/api/insert_group_admin.ts
@@ -10,19 +10,24 @@ export const insert_group_admin = async (req: any, res: any): Promise<void> => {
 
     const groupId = req.proxyParamsGroupId;
 
-    // Send new invitation
-    const newId = v4();
-    const insertRes = await db.query(
-      `INSERT INTO admins (id, email, is_superadmin) VALUES ($1, lower($2), $3) ON CONFLICT (email) DO NOTHING RETURNING id`,
-      [newId, email, false],
-    );
-
-    if (insertRes.rowCount === 1) {
-      await db.query(
-        'INSERT INTO admin_groups (admin_id, group_id) VALUES ($1,$2) ON CONFLICT (admin_id, group_id) DO NOTHING',
-        [insertRes.rows[0].id, groupId],
+    const selectRes = await db.query('SELECT id FROM admins WHERE email=$1', [email]);
+    var id = '';
+    if (selectRes.rows.length === 0) {
+      // Send new invitation
+      const newId = v4();
+      const insertRes = await db.query(
+        `INSERT INTO admins (id, email, is_superadmin) VALUES ($1, lower($2), $3) RETURNING id`,
+        [newId, email, false],
       );
+      id = insertRes.rows[0].id;
+    } else {
+      id = selectRes.rows[0].id;
     }
+
+    await db.query(
+      'INSERT INTO admin_groups (admin_id, group_id) VALUES ($1,$2) ON CONFLICT (admin_id, group_id) DO NOTHING',
+      [id, groupId],
+    );
     res.status(200).end();
   } catch (e) {
     logError('insert_group_admin', e);
