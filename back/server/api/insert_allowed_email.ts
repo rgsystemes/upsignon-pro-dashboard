9faaa941e8f24df59b8e150566432e9d71879a3f
Gireg <giregk@upsignon.eu>
2025-01-28 16:23:47 +0100

    rename vars to provide hint to snyk

--- back/server/api/insert_allowed_email.ts
+++ back/server/api/insert_allowed_email.ts
@@ -4,15 +4,15 @@ import { logError } from '../helpers/logger';
 
 export const insert_allowed_email = async (req: any, res: any): Promise<void> => {
   try {
-    const newEmailPattern = req.body.newPattern;
+    const joiValidatedNewEmailPattern = req.body.newPattern;
     try {
-      Joi.assert(newEmailPattern, Joi.string().required());
+      Joi.assert(joiValidatedNewEmailPattern, Joi.string().required());
     } catch (e) {
       console.error(e);
       return res.status(400).end();
     }
     await db.query(`INSERT INTO allowed_emails (pattern, group_id) VALUES (lower($1), $2)`, [
-      newEmailPattern.trim().toLowerCase(),
+      joiValidatedNewEmailPattern.trim().toLowerCase(),
       req.proxyParamsGroupId,
     ]);
     res.status(200).end();
--- back/server/api/update_allowed_email.ts
+++ back/server/api/update_allowed_email.ts
@@ -4,15 +4,15 @@ import { logError } from '../helpers/logger';
 
 export const update_allowed_email = async (req: any, res: any): Promise<void> => {
   try {
-    const newEmailPattern = req.body.updatedPattern;
+    const joiValidatedNewEmailPattern = req.body.updatedPattern;
     try {
-      Joi.assert(newEmailPattern, Joi.string().required());
+      Joi.assert(joiValidatedNewEmailPattern, Joi.string().required());
     } catch (e) {
       console.error(e);
       return res.status(400).end();
     }
     await db.query(`UPDATE allowed_emails SET pattern=lower($1) WHERE id=$2 AND group_id=$3`, [
-      newEmailPattern.trim().toLowerCase(),
+      joiValidatedNewEmailPattern.trim().toLowerCase(),
       req.body.allowedEmailId,
       req.proxyParamsGroupId,
     ]);
