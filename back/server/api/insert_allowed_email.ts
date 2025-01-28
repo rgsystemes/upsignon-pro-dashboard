a788ec25b9c4afe4611dc4ee56d3889ae4c81d8f
Gireg <giregk@upsignon.eu>
2025-01-28 16:08:10 +0100

    fix input prototype pollution

--- back/server/api/insert_allowed_email.ts
+++ back/server/api/insert_allowed_email.ts
@@ -1,10 +1,18 @@
+import Joi from 'joi';
 import { db } from '../helpers/db';
 import { logError } from '../helpers/logger';
 
 export const insert_allowed_email = async (req: any, res: any): Promise<void> => {
   try {
+    const newEmailPattern = req.body.newPattern;
+    try {
+      Joi.assert(newEmailPattern, Joi.string().required());
+    } catch (e) {
+      console.error(e);
+      return res.status(400).end();
+    }
     await db.query(`INSERT INTO allowed_emails (pattern, group_id) VALUES (lower($1), $2)`, [
-      req.body.newPattern.trim().toLowerCase(),
+      newEmailPattern.trim().toLowerCase(),
       req.proxyParamsGroupId,
     ]);
     res.status(200).end();
--- back/server/api/update_allowed_email.ts
+++ back/server/api/update_allowed_email.ts
@@ -1,10 +1,18 @@
+import Joi from 'joi';
 import { db } from '../helpers/db';
 import { logError } from '../helpers/logger';
 
 export const update_allowed_email = async (req: any, res: any): Promise<void> => {
   try {
+    const newEmailPattern = req.body.updatedPattern;
+    try {
+      Joi.assert(newEmailPattern, Joi.string().required());
+    } catch (e) {
+      console.error(e);
+      return res.status(400).end();
+    }
     await db.query(`UPDATE allowed_emails SET pattern=lower($1) WHERE id=$2 AND group_id=$3`, [
-      req.body.updatedPattern.trim().toLowerCase(),
+      newEmailPattern.trim().toLowerCase(),
       req.body.allowedEmailId,
       req.proxyParamsGroupId,
     ]);
