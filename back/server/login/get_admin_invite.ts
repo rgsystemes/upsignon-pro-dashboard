7c7136ef5e658e3890baaf225a86c16cfe566324
Gireg <giregk@upsignon.eu>
2025-02-26 14:03:56 +0100

    fix multiple clicks on login form, fix mail sending when not admin, fix reuse token if still valid instead of recreating it

--- back/server/login/get_admin_invite.ts
+++ back/server/login/get_admin_invite.ts
@@ -8,20 +8,33 @@ export const getAdminInvite = async (req: any, res: any): Promise<void> => {
   try {
     const adminEmail = inputSanitizer.getLowerCaseString(req.body.adminEmail);
     if (!adminEmail) return res.status(400).end();
-    const token = v4();
-    const tokenExpiresAt = new Date();
-    const ttl = ttlMinutes * 60 * 1000;
-    tokenExpiresAt.setTime(tokenExpiresAt.getTime() + ttl);
-    const dbRes = await db.query(
-      'UPDATE admins SET token=$1, token_expires_at=$2 WHERE email=$3 RETURNING id',
-      [token, tokenExpiresAt, adminEmail],
-    );
-    sendAdminInvite(adminEmail, token, tokenExpiresAt, null);
-    if (dbRes.rowCount === 1) {
-      return res.status(200).json({ success: true });
-    } else {
+    const getRes = await db.query('SELECT token, token_expires_at FROM admins WHERE email=$1', [
+      adminEmail,
+    ]);
+    if (getRes.rowCount == 0) {
       return res.status(200).json({ success: false });
     }
+    let admRes = getRes.rows[0];
+    let token, tokenExpiresAt;
+    if (
+      !admRes.token ||
+      !admRes.token_expires_at ||
+      admRes.token_expires_at.getTime() < Date.now()
+    ) {
+      token = v4();
+      tokenExpiresAt = new Date();
+      const ttl = ttlMinutes * 60 * 1000;
+      tokenExpiresAt.setTime(tokenExpiresAt.getTime() + ttl);
+      const dbRes = await db.query(
+        'UPDATE admins SET token=$1, token_expires_at=$2 WHERE email=$3 RETURNING id',
+        [token, tokenExpiresAt, adminEmail],
+      );
+    } else {
+      token = getRes.rows[0].token;
+      tokenExpiresAt = getRes.rows[0].token_expires_at;
+    }
+    sendAdminInvite(adminEmail, token, tokenExpiresAt, null);
+    return res.status(200).json({ success: true });
   } catch (e) {
     logError('getAdminInvite', e);
     res.status(200).json({ success: false });
