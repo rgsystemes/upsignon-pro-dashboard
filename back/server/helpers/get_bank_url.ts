d6bd4a44a6f63d485183b87b47deb0aee8c94965
Gireg <giregk@upsignon.eu>
2025-04-08 12:15:01 +0200

    use the new simplified pro-setup url

--- back/server/helpers/get_bank_url.ts
+++ back/server/helpers/get_bank_url.ts
@@ -14,7 +14,7 @@ export const get_bank_url = async (req: any, res: any): Promise<void> => {
     const bankRes = await db.query('SELECT public_id FROM groups WHERE id=$1', [
       req.proxyParamsGroupId,
     ]);
-    const link = `https://app.upsignon.eu/pro-setup?url=${encodeURIComponent(url + '/' + bankRes.rows[0].public_id)}`;
+    const link = url + '/' + bankRes.rows[0].public_id;
 
     res.status(200).json({ url: link });
   } catch (e) {
