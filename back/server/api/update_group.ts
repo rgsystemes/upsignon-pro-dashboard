c0bc72b41bc0cedb85253b7c8ed98229d71cc643
Gireg <giregk@upsignon.eu>
2025-06-25 10:31:21 +0200

    hide secret from fontend

--- back/server/api/update_group.ts
+++ back/server/api/update_group.ts
@@ -16,6 +16,18 @@ export const update_group = async (req: any, res: any): Promise<void> => {
       ]);
     }
     if (req.body.msEntraConfig) {
+      if (
+        typeof req.body.msEntraConfig.clientSecret === 'string' &&
+        /^[*]+$/.test(req.body.msEntraConfig.clientSecret)
+      ) {
+        // clientSecret contains only asterisques, do not update it.
+        const dbRes = await db.query(`SELECT ms_entra_config FROM groups WHERE id=$1`, [
+          req.proxyParamsGroupId,
+        ]);
+        const previousConfig = dbRes.rows[0].ms_entra_config || {};
+        req.body.msEntraConfig.clientSecret = previousConfig.clientSecret;
+      }
+
       await db.query('UPDATE groups SET ms_entra_config=$1 WHERE id=$2', [
         req.body.msEntraConfig,
         req.proxyParamsGroupId,
