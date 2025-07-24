eceeda354d222115c6ca1b5d69f0824ddcc33e7a
Gireg <giregk@upsignon.eu>
2025-07-24 15:43:28 +0200

    update microsoftConfigUrl for generic url

--- back/server/api/sso/sso_constants.ts
+++ back/server/api/sso/sso_constants.ts
@@ -1,3 +1,3 @@
 export const microsoftConfigUrl =
-  'https://login.microsoftonline.com/septeo.com/v2.0/.well-known/openid-configuration';
+  'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration';
 export const microsoftClientId = 'a58bc65d-c84f-4526-9057-6d7722ff8593';
