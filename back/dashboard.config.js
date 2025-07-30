0cf00c90bee62ec446c67707de4d1e044ee0b6ac
Gireg <giregk@upsignon.eu>
2025-07-30 12:43:44 +0200

    remove unused dotenv import in config.js

--- back/dashboard.config.js
+++ back/dashboard.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, './.env') });
 
 module.exports = {
   apps: [
