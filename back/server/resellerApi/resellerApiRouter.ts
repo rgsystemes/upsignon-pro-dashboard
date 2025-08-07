08d1fd4c61929e4003daa90860dfe6f11c96f436
Gireg <giregk@upsignon.eu>
2025-08-07 16:48:23 +0200

    11024 add licence vue and assignment interface

--- back/server/resellerApi/resellerApiRouter.ts
+++ back/server/resellerApi/resellerApiRouter.ts
@@ -5,6 +5,8 @@ import { delete_bank } from './banks/delete_bank';
 import { admins } from './admins/admins';
 import { insert_admin } from './admins/insert_admin';
 import { delete_admin } from './admins/delete_admin';
+import { get_licences } from '../api/get_licences';
+import { licenceAssign } from '../helpers/licence_assign';
 
 export const resellerApiRouter = express.Router();
 
@@ -17,3 +19,7 @@ resellerApiRouter.post('/delete-bank/:id', delete_bank);
 resellerApiRouter.get('/reseller-admins', admins);
 resellerApiRouter.post('/insert-reseller-admin', insert_admin);
 resellerApiRouter.post('/delete-reseller-admin/:adminId', delete_admin);
+
+// LICENCES
+resellerApiRouter.get('/licences', (req, res) => get_licences(req, res, false));
+resellerApiRouter.post('/licences-assign', (req, res) => licenceAssign(req, res, false));
