1090686ee99951534940fb18fbb01bfc37b58c50
Gireg <giregk@upsignon.eu>
2025-08-07 17:17:15 +0200

    11024 fix nb_licences column on superadmin banks view

--- back/server/superadminapi/get_banks.ts
+++ back/server/superadminapi/get_banks.ts
@@ -10,16 +10,54 @@ export const get_banks = async (req: any, res: any): Promise<void> => {
         banks.name,
         banks.settings,
         (SELECT count(users.id) FROM users WHERE users.bank_id=banks.id) AS nb_users,
-        0 as nb_licences,
         banks.reseller_id,
         resellers.name as reseller_name
       FROM banks
       LEFT JOIN resellers ON resellers.id=banks.reseller_id
       ORDER BY name ASC`,
     );
+    const banks = dbRes.rows;
+    for (let i = 0; i < banks.length; i++) {
+      const nbLicences = await countAvailableLicences(banks[i].id);
+      banks[i].nb_licences = nbLicences;
+    }
     res.status(200).send(dbRes.rows);
   } catch (e) {
     logError('get_banks', e);
     res.status(400).end();
   }
+};
+
+const countAvailableLicences = async (bankId: number) => {
+  const internalLicences = await db.query(
+    `SELECT
+    il.nb_licences
+    FROM internal_licences AS il
+    INNER JOIN external_licences AS el ON il.external_licences_id=el.ext_id
+    WHERE il.bank_id=$1
+    AND ((el.is_monthly=true AND el.to_be_renewed != false) OR (el.valid_from <= current_timestamp(0) AND current_timestamp(0) < el.valid_until))
+    `,
+    [bankId],
+  );
+  const externalLicences = await db.query(
+    `SELECT
+    el.nb_licences
+    FROM external_licences AS el
+    WHERE el.bank_id=$1
+    AND ((el.is_monthly=true AND el.to_be_renewed != false) OR (el.valid_from <= current_timestamp(0) AND current_timestamp(0) < el.valid_until))
+    `,
+    [bankId],
+  );
+  let bankSpecificNbLicences = 0;
+  if (internalLicences.rows.length > 0) {
+    for (let i = 0; i < internalLicences.rows.length; i++) {
+      bankSpecificNbLicences += internalLicences.rows[i].nb_licences;
+    }
+  }
+  if (externalLicences.rows.length > 0) {
+    for (let i = 0; i < externalLicences.rows.length; i++) {
+      bankSpecificNbLicences += externalLicences.rows[i].nb_licences;
+    }
+  }
+  return bankSpecificNbLicences;
 };
