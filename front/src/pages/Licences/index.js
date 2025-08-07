1af8dd7a9762cf9f1a795c21a1472889c448cbf3
Gireg <giregk@upsignon.eu>
2025-08-07 10:00:58 +0200

    11024 show superadmin licences

--- front/src/pages/Licences/index.js
+++ front/src/pages/Licences/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { bankId } from '../../helpers/env';
+import { bankId, isSaasServer } from '../../helpers/env';
 import { i18n } from '../../i18n/i18n';
 import { bankUrlFetch } from '../../helpers/urlFetch';
 
@@ -14,17 +14,8 @@ export class Licences extends React.Component {
       const res = await bankUrlFetch('/api/get-licences', 'POST', null);
       if (res) {
         this.setState({
-          licences: res.sort((a, b) => {
-            if (a.bankName < b.bankName) return -1;
-            if (a.bankName > b.bankName) return 1;
-            if (a.masterBank < b.masterBank) return -1;
-            if (a.masterBank > b.masterBank) return 1;
-            if (a.valid_until < b.valid_until) return -1;
-            if (a.valid_until > b.valid_until) return -1;
-            if (a.valid_from < b.valid_from) return -1;
-            if (a.valid_from > b.valid_from) return -1;
-            return 0;
-          }),
+          internalLicences: res.internalLicences,
+          externalLicences: res.externalLicences,
         });
       }
     } catch (e) {
@@ -35,9 +26,47 @@ export class Licences extends React.Component {
     this.fetchLicences();
   }
 
+  classNameForLicence = (l) => {
+    try {
+      const isExpiredNotRenewed =
+        l.to_be_renewed && !l.is_monthly && new Date(l.valid_until).getTime() < Date.now();
+      let willExpireSoon = false;
+      if (l.valid_until && l.to_be_renewed) {
+        let expDateMinus3Month = new Date(l.valid_until);
+        expDateMinus3Month.setMonth(expDateMinus3Month.getMonth() - 3);
+        willExpireSoon = expDateMinus3Month.getTime() < Date.now() && l.to_be_renewed;
+      }
+      const isExpiredNotToRenew =
+        !l.to_be_renewed && new Date(l.valid_until).getTime() < Date.now();
+      let className = null;
+      if (isExpiredNotRenewed) className = 'redrow';
+      else if (willExpireSoon) className = 'orangerow';
+      else if (isExpiredNotToRenew) className = 'greyrow';
+      return className;
+    } catch (e) {
+      console.error(e, l);
+      return null;
+    }
+  };
+  licenceRow = (l, idPrefix) => {
+    const className = this.classNameForLicence(l);
+    return (
+      <tr key={`${idPrefix}${l.id}`} className={className}>
+        {isSaasServer && <td>{l.reseller_name}</td>}
+        <td>{l.bank_name}</td>
+        <td>{l.nb_licences}</td>
+        <td>
+          <input type="checkbox" checked={l.is_monthly} disabled />
+        </td>
+        <td>{new Date(l.valid_from).toLocaleDateString()}</td>
+        <td>{l.valid_until ? new Date(l.valid_until).toLocaleDateString() : '-'}</td>
+        <td>{l.to_be_renewed ? i18n.t('yes') : i18n.t('no')}</td>
+      </tr>
+    );
+  };
   render() {
     const hasLicences =
-      this.state.licences?.length > 0 && this.state.licences.some((l) => l.licences.length > 0);
+      this.state.internalLicences?.length > 0 || this.state.externalLicences?.length > 0;
     return (
       <div className="page">
         <h1>{i18n.t('menu_licences')}</h1>
@@ -54,43 +83,18 @@ export class Licences extends React.Component {
             <table style={{ marginBottom: 20 }}>
               <thead>
                 <tr>
-                  <th></th>
+                  {isSaasServer && <th>{i18n.t('licences_reseller_name')}</th>}
                   <th>{i18n.t('licences_bank_name')}</th>
                   <th>{i18n.t('licences_nb')}</th>
+                  <th>{i18n.t('licences_is_montly')}</th>
                   <th>{i18n.t('licences_valid_from')}</th>
                   <th>{i18n.t('licences_valid_until')}</th>
                   <th>{i18n.t('licences_to_be_renewed')}</th>
                 </tr>
               </thead>
               <tbody>
-                {this.state.licences.map((l, i) => {
-                  return (
-                    <React.Fragment key={i}>
-                      {l.licences.map((ll, ii) => {
-                        const isExpiredNotRenewed =
-                          new Date(ll.valid_until).getTime() < Date.now() && ll.to_be_renewed;
-                        let expDateMinus3Month = new Date(ll.valid_until);
-                        expDateMinus3Month.setMonth(expDateMinus3Month.getMonth() - 3);
-                        const willExpireSoon =
-                          expDateMinus3Month.getTime() < Date.now() && ll.to_be_renewed;
-                        let className = null;
-                        if (isExpiredNotRenewed) className = 'redrow';
-                        else if (willExpireSoon) className = 'orangerow';
-                        else if (!ll.to_be_renewed) className = 'greyrow';
-                        return (
-                          <tr key={i} className={className}>
-                            <td>{l.masterBank}</td>
-                            <td>{l.bankName}</td>
-                            <td>{ll.nb_licences}</td>
-                            <td>{new Date(ll.valid_from).toLocaleDateString()}</td>
-                            <td>{new Date(ll.valid_until).toLocaleDateString()}</td>
-                            <td>{ll.to_be_renewed ? i18n.t('yes') : i18n.t('no')}</td>
-                          </tr>
-                        );
-                      })}
-                    </React.Fragment>
-                  );
-                })}
+                {this.state.internalLicences.map((l, i) => this.licenceRow(l, 'IL'))}
+                {this.state.externalLicences.map((l, i) => this.licenceRow(l, 'EL'))}
               </tbody>
             </table>
           </div>
