2c409a428ff656241e69626ed1856541dcaa61ac
giregk <giregk@users.noreply.github.com>
2025-09-26 14:29:04 +0200

    11024 add comment on Licences flows

--- back/server/superadminapi/start_pull_licences.ts
+++ back/server/superadminapi/start_pull_licences.ts
@@ -3,8 +3,12 @@ import { logError } from '../helpers/logger';
 import { db } from '../helpers/db';
 import { proxiedFetch } from '../helpers/xmlHttpRequest';
 
-/// This route calls the upsignon-pro-server start-licence-pulling route
+// NB: Licence flows
+// - PUSH FLOW (licence update/creation): upsignon-adv-dashboard -> upsignon-pro-server/licences
+// - AUTO PULL FLOW (pro server cron and start): upsignon-pro-server -> upsignon-perso-server/pull-licences -> upsignon-adv-dashboard/pull-licences
+// - MANUAL PULL FLOW: upsignon-pro-dashboard/start-pull-licences -> upsignon-perso-server/pull-licences -> upsignon-adv-dashboard/pull-licences
 
+/// This route calls the upsignon-pro-server start-licence-pulling route
 export const startPullLicences = async (req: Request, res: Response): Promise<void> => {
   try {
     const settingsRes = await db.query(
